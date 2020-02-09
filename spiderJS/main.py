c6291d7d754046d7d1c9db18f9405519ea941b79
Dj-Polyester <anonymous_571@outlook.com>
2020-02-10 01:17:31 +0300

    update

--- spiderJS/main.py
+++ spiderJS/main.py
@@ -1,22 +1,30 @@
 import os
 
-data = input("Please input the word you would like to search for. CTRL+C to exit.\n")
-command= "node scrape.js {}".format(data)
-print("Searching for main...")
-os.system(command)
-print("Done!")
+def getIn():
+    data = input("Please input the word you would like to search for. CTRL+C to exit.\n")
+    command= "node scrape.js {}".format(data)
+    print("Searching for main...")
+    os.system(command)
+    print("Done!")
 
-with open("filename.js") as fileNameContents:
-    sum=0
+def getTotal():
+    fileNameContents=open("filename.js")
     rawName=fileNameContents.readline().split("=")[1]
-    
     fileName=rawName.strip()
     fileName=fileName.strip("\"")
-    
-    with open(fileName) as f:
-        for line in f:
-            alist=line.split(" ")
-            if len(alist) > 4:
-                num = int(alist[4])
-                sum+=num
+    fileNameContents.close()
+
+    f=open(fileName,"r+")
+    sum=0
+    for line in f:
+        alist=line.split(" ")
+        if len(alist) > 4:
+            num = int(alist[4])
+            sum+=num
+    f.write("\nFound {} total".format(sum))
     print("\nFound {} total".format(sum))
+    f.close()
+
+if __name__ == "__main__":
+    getIn()
+    getTotal()
